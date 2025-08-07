c585853b11eb1274975b35eed54ce3fba15de5ba
script <154348722+ScriptsHub07@users.noreply.github.com>
2025-08-07 12:53:01 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -36,3 +36,8 @@ def verificar():
 
     return jsonify({'status': 'valida', 'mensagem': 'Licença válida'}), 200
 
+    if __name__ == '__main__':
+        import os
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
+
